331e4b4bc33085c7001fb6463140ac3024d50452
Wayne.Chiu <Wayne.Chiu@advantech.com.tw>
2021-12-15 11:35:24 +0800

    update with LSTM (RNN)

--- dl/Store_Sales/store-sales-time-series-forecasting.py
+++ dl/Store_Sales/store-sales-time-series-forecasting.py
@@ -1,13 +1,14 @@
 
 # refer https://www.kaggle.com/c/titanic
 # from keras.activations import deserialize
+from datetime import date
 from typing import Optional
 import numpy as np # linear algebra
 import pandas as pd
 from scipy.sparse import data
 
 serial_number  = 0
-
+DataPath="./kaggle/input/store-sales-time-series-forecasting/"
 def serial_number_plus():
     global serial_number
     serial_number += 1
@@ -33,7 +34,7 @@ def license_alarm():
     print(decode('+--------------------------------------------------------------------------%'))
     print(decode('|'), '              Store-Sales-time-series-forecasting  - Practice           ', decode('|'))
     print(decode('|'), ' refer https://www.kaggle.com/c/store-sales-time-series-forecasting     ', decode('|'))
-    print(decode('|'), '                              v0.01                                     ', decode('|'))
+    print(decode('|'), '                              v0.02                                     ', decode('|'))
     print(decode('|'), '            Copyright (c) Wayne Chiu 2021. All Rights Reserved          ', decode('|'))
     print(decode('k--------------------------------------------------------------------------f'))
     print(decode('|'), '                                                                        ', decode('|'))
@@ -62,93 +63,224 @@ from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.python.keras.layers.core import Dropout 
+
+#EDA: Exploratory Data Analysis
+def data_eda(src_df):
+    print(src_df.head(5))
+    print(decode('------')+"Check none (NULL)"+decode('------'))
+    print(src_df.isnull().sum())
+    print(decode('------')+"Check NA or NAN (Not Available, Not A Number)"+decode('------'))
+    print(src_df.isna().sum())
+
+
+train_df = pd.read_csv(DataPath+"train.csv", index_col=0)
+print(decode('------')+"train_df"+decode('------'))
+# data_eda(train_df)
+test_df = pd.read_csv(DataPath+"test.csv", index_col=0)
+print(decode('------')+"test_df"+decode('------'))
+# data_eda(test_df)
+oil_df = pd.read_csv(DataPath+"oil.csv")
+print(decode('------')+"oil_df"+decode('------'))
+# data_eda(oil_df)
+holiday_event_df = pd.read_csv(DataPath+"holidays_events.csv")
+print(decode('------')+"holiday_event_df"+decode('------'))
+# data_eda(holiday_event_df)
+transaction_df = pd.read_csv(DataPath+"transactions.csv")
+print(decode('------')+"transaction_df"+decode('------'))
+# data_eda(transaction_df)
+store_df = pd.read_csv(DataPath+"stores.csv")
+print(decode('------')+"store_df"+decode('------'))
+# data_eda(store_df)
+
+print(decode('------')+"combine data"+decode('------'))
+#combine data (holidays_events/oil) base on date
+pre_data = train_df.merge(holiday_event_df, on = 'date' ,how='left')
+pre_data = pre_data.merge(oil_df, on = 'date' ,how='left')
+#combine data (store_df) base on store_nbr
+pre_data = pre_data.merge(store_df, on = 'store_nbr' ,how='left')
+#combine data (transaction_df) base on date,store_nbr
+pre_data = pre_data.merge(transaction_df, on = ['date','store_nbr'] ,how='left')
+
+#Data modify
+#rename (type_x base on "holidays_events.csv")
+#       (type_y base on "stores.csv")
+pre_data =pre_data.rename(columns={"type_x" : "event_type", "type_y" : "store_type"})
+
+#change date data
+pre_data['date'] = pd.to_datetime(pre_data['date'])
+pre_data['year'] = pre_data['date'].dt.year
+pre_data['month'] = pre_data['date'].dt.month
+pre_data['week'] = pre_data['date'].dt.isocalendar().week
+pre_data['quarter'] = pre_data['date'].dt.quarter
+pre_data['day_of_week'] = pre_data['date'].dt.day_name()
+
+#group by 'store_type' and get mean.
+storetype_sales_df = pre_data.groupby('store_type').agg({"sales" : "mean"}).reset_index().sort_values(by='sales', ascending=False)
+# storetype_sales_df.to_csv("store_sale.csv")
+
+#group by 'family' and get mean.
+family_sales_df = pre_data.groupby('family').agg({"sales" : "mean"}).reset_index().sort_values(by='sales', ascending=False)[:10]
+# family_sales_df.to_csv("family_sales.csv")
+
+#group by 'cluster' and get mean
+cluster_sales_df = pre_data.groupby('cluster').agg({"sales" : "mean"}).reset_index()
+# cluster_sales_df.to_csv("cluster_sales.csv")
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+def show_eda_chart():
+    #color 
+    family_sales_df['color'] = '#008000'
+    family_sales_df['color'][:3] = '#00FF00'
+    cluster_sales_df['color'] = '#00FF00'
+    
+    #chart diagram
+    figures = make_subplots(rows=2, cols=2, specs=[[{"type" : "bar"}, {"type" : "pie"}],[{"colspan" : 2}, None]],
+     column_widths=[0.7, 0.3], vertical_spacing=0, horizontal_spacing=0.02,
+      subplot_titles=("Top 10 highest Product Sales", "Highest Sales in Stores", "Clusters Vs Sales"))
+    
+    figures.add_trace(go.Bar(x=family_sales_df['sales'], y =family_sales_df['family'],
+                    marker=dict(color=family_sales_df['color']), name='Family', orientation='h'),
+                    row=1, col=1)
+    # pre_data.to_csv("testwayne.csv")
+    figures.add_trace(go.Pie(values=storetype_sales_df['sales'], labels=storetype_sales_df['store_type'], name='Store type',
+                    marker=dict(colors=['#006400', '#008000','#228B22','#00FF00','#7CFC00','#00FF7F']), hole=0.7,
+                    hoverinfo='label+percent+value', textinfo='label'), 
+                    row=1, col=2)
+    figures.add_trace(go.Bar(x=cluster_sales_df['cluster'], y=cluster_sales_df['sales'], 
+                         marker=dict(color= cluster_sales_df['color']), name='Cluster'), 
+                         row=2, col=1)
+    
+    figures.show()
+
+# show_eda_chart()
+
 import matplotlib.pyplot as plt
 
-def showplt(history):
-    pd.DataFrame(history.history).plot(figsize=(8, 5))
-    plt.grid(True)
-    plt.gca().set_ylim(0, 1)  # Set the vertical range to [0-1]
+def show_oil_plt(oil_data):
+    plt.plot(oil_data.set_index('date').dcoilwtico, color='green', label=f"oil_price")
+    plt.title("Oil price vs Days")
+    plt.xlabel("Days")
+    plt.ylabel("Oil Price")
+    plt.legend()
     plt.show()
 
-def show_train_history(train_history, train, validation):
-    plt.plot(train_history.history[train], label=train)
-    plt.plot(train_history.history[validation], label=validation)
-    plt.title("Train History")
-    plt.ylabel(train)
-    plt.xlabel('Epoch')
+# show_oil_plt(pre_data)
+
+#date sales (per day) modify
+date_sales_per_day_df = pre_data.groupby('date').agg({'sales':'mean'}).reset_index()
+date_sales_per_day_df['weekly_avg_sales'] = date_sales_per_day_df['sales'].ewm(span=7, adjust=False).mean()
+# date_sales_per_day_df.to_csv("date_sale_per_day.csv")
+# date_sales_per_day_df.head()
+
+#transaction (per day)
+transaction_per_day_df = pre_data.groupby('date').agg({'transactions':'mean'}).reset_index()
+transaction_per_day_df['weekly_avg_transactions'] = transaction_per_day_df['transactions'].ewm(span=7,adjust=False).mean()
+# transaction_per_day_df.to_csv("transaction_per_day.csv")
+transaction_per_day_df.head()
+
+def show_sales_oil_plt():
+    figures=make_subplots()
+
+    figures.add_trace(go.Scatter(x=date_sales_per_day_df.date,y=date_sales_per_day_df.sales,name="Sales"))
+    figures.add_trace(go.Scatter(x=date_sales_per_day_df.date,y=date_sales_per_day_df.weekly_avg_sales,name="Weekly Sales"))
+    
+    
+    figures.add_trace(go.Scatter(x=oil_df.date,y=oil_df.dcoilwtico,name="Oil Price"))
+    
+    figures.update_layout(autosize=True,width=900,height=500,title_text="Variation of Sales and Oil Price Through Time")
+    figures.update_xaxes(title_text="Days")
+    figures.update_yaxes(title_text="Prices")
+    figures.show()
+
+show_sales_oil_plt()
+
+def show_transaction_oil_plt():
+    figures=make_subplots()
+    
+    figures.add_trace(go.Scatter(x=transaction_per_day_df.date,y=transaction_per_day_df.transactions,name="Transactions"))
+    figures.add_trace(go.Scatter(x=transaction_per_day_df.date,y=transaction_per_day_df.weekly_avg_transactions,name="Weekly Transactions"))
+    
+    figures.add_trace(go.Scatter(x=oil_df.date,y=oil_df.dcoilwtico,name="Oil Price"))
+    
+    figures.update_layout(autosize=True,width=900,height=500,title_text="Variation Transactions and Oil Price Through Time")
+    figures.update_xaxes(title_text="Days")
+    figures.update_yaxes(title_text="Prices")
+    figures.show()
+
+show_transaction_oil_plt()
+
+import seaborn as sns
+
+#pearson correlation  (sales, transaction, oil)
+def pearson_correlation_4_oil():
+    oil_df['sales'] = date_sales_per_day_df['sales']
+    oil_df['transactions'] = transaction_per_day_df['transactions']
+    plt.figure(figsize=(11,3))
+    sns.heatmap(oil_df.corr(), annot=True)
     plt.show()
 
-
-# data processing, csv file I/O
-# from tensorflow.keras.utils import np_utils
-
-train_df = pd.read_csv("./kaggle/input/digit-recognizer/train.csv")
-test_df = pd.read_csv("./kaggle/input/digit-recognizer/test.csv")
-
-train_df.head
-test_df.head
-
-
-#==========2 filter train data x and y
-# 
-X_train_data = train_df.drop('label',axis=1)/255
-y_train_data= pd.get_dummies(train_df.label)
-
-X_test_data = test_df/255
-
-X_train, X_valid, y_train, y_valid = train_test_split(X_train_data, y_train_data, train_size=0.8, test_size=0.2, random_state=50)
+pearson_correlation_4_oil()
 
 
 
-X_train = X_train.values.reshape(-1, 28, 28, 1)
-X_valid = X_valid.values.reshape(-1, 28, 28, 1)
-X_test  = X_test_data.values.reshape(-1, 28, 28, 1)
+#==================================================================================
 
-EPOCH = 1
-LEARNING_RATE = 0.0005
 
-model = Sequential()
+train_data = train_df.groupby(['date']).agg({'sales':'mean', 'onpromotion':'mean'})
+train_data.tail()
 
-model.add(Conv2D(32, (5,5), activation="relu", padding="same", data_format="channels_last", input_shape=(28,28,1)))
-model.add(Conv2D(32, (5,5), activation="relu", padding="same", data_format="channels_last"))
-model.add(MaxPool2D(pool_size=(2,2), data_format="channels_last"))
-model.add(Dropout(0.5))
 
-model.add(Conv2D(64, (5,5), activation="relu", padding="same", data_format="channels_last"))
-model.add(Conv2D(64, (5,5), activation="relu", padding="same", data_format="channels_last"))
-model.add(MaxPool2D(pool_size=(2,2), data_format="channels_last"))
-model.add(Dropout(0.5))
 
-model.add(Flatten())
-model.add(Dense(128,activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation="softmax"))
+x_train = train_data.copy()
+y_train = train_data.sales.copy()
 
-Decay = 5* LEARNING_RATE / EPOCH
-Optimizer= RMSprop(learning_rate=LEARNING_RATE, rho=0.9, epsilon=1e-08, decay=Decay)
-model.compile(optimizer=Optimizer, loss="categorical_crossentropy", metrics=['accuracy'])
+num_feature_input = len(x_train.columns)
+history_input = 30
 
+from keras.preprocessing.sequence import TimeseriesGenerator
+generator = TimeseriesGenerator(x_train, y_train, length=history_input, batch_size = 1)
+
+
+from keras.layers import LSTM
+
+def Multi_Step_LSTM_model():
+    
+    # Use Keras sequential model
+    model = Sequential()    
+    
+    # First LSTM layer with Dropout regularisation; Set return_sequences to True to feed outputs to next layer
+    model.add(LSTM(units = 50, activation='relu', return_sequences = True, input_shape = (history_input, num_feature_input))) 
+    model.add(Dropout(0.2))
+    
+    # Second LSTM layer with Dropout regularisation; Set return_sequences to True to feed outputs to next layer
+    model.add(LSTM(units = 50,  activation='relu', return_sequences = True))                                    
+    model.add(Dropout(0.2))
+    
+    # Final LSTM layer with Dropout regularisation; Set return_sequences to False since now we will be predicting with the output layer
+    model.add(LSTM(units = 50))
+    model.add(Dropout(0.2))
+    
+    # The output layer with linear activation to predict Open stock price
+    model.add(Dense(units=1, activation = "linear"))
+    
+    return model
+
+
+
+model = Multi_Step_LSTM_model()
 model.summary()
 
-lr_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
-train_history = model.fit(X_train, y_train, epochs=EPOCH, validation_data=(X_valid,y_valid), verbose=2, callbacks=[lr_reduction])
+model.compile(optimizer='adam', loss='mean_squared_error', metrics = ['accuracy'])
 
-show_train_history(train_history, 'accuracy', 'val_accuracy')
+model.fit(generator, steps_per_epoch=len(generator), epochs=20, verbose=2)
+
+full_dataset = pd.concat([train_data, test_df], ignore_index=True, sort=False)
+full_dataset.to_csv("checkTest.csv")
+full_dataset = full_dataset.iloc[3000887-5:,:]
 
 
-show_train_history(train_history, 'loss', 'val_loss')
 
-val_acc_per_epoch = train_history.history['val_accuracy']
-best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch))+1
-print(best_epoch, max(val_acc_per_epoch))
-
-results = model.predict(X_test)
-results = np.argmax(results, axis=1)
-results = pd.Series(results, name="Label")
-
-submission = pd.concat([pd.Series(range(1,28001),name = "ImageId"),results],axis = 1)
-
-submission.to_csv("submission.csv",index=False)
-print("Your submission was successfully saved!")
-# print(X_train)
-# y_train.head
+full_dataset = full_dataset.groupby(['date']).agg({'sales':'mean', 'onpromotion':'mean'})
+full_dataset.to_csv("checkTest12.csv")
